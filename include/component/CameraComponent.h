c1f9dde75b162b020851000514c0f72e85e68c8c
Michael Stolls <michael.stolls@gameloft.com>
2019-03-29 12:08:51 -0400

    adding the starting of camera component

---
+++ include/component/CameraComponent.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "manager/GameObjectManager.h"
+
+namespace GameEngine
+{
+	class CameraComponent : public IComponent
+	{
+	public:
+											CameraComponent();
+											~CameraComponent();
+											
+	private:
+
+	};
+}
---
+++ src/component/CameraComponent.cpp
@@ -0,0 +1,14 @@
+#include "component/CameraComponent.h"
+
+namespace GameEngine
+{
+	CameraComponent::CameraComponent() : IComponent()
+	{
+		m_Name = "Camera Component";
+	}
+
+	CameraComponent::~CameraComponent()
+	{
+
+	}
+}
